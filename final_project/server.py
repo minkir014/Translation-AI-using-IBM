dd4e3ac014d5249946ee6c4cc33d92f6e6995130
Mina <mina.fawzy02@eng-st.cu.edu.eg>
2022-10-02 02:56:05 +0200

    Finalized the Server

--- final_project/server.py
+++ final_project/server.py
@@ -1,28 +1,32 @@
+"""
+This is the server for the translation service
+"""
+
 from flask import Flask, render_template, request
 from machinetranslation import translator
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
+def english_to_french():
     """
     This represents the Englsh to French Translation Route
     """
 
-    textToTranslate = request.args.get('textToTranslate')
-    return translator.english_to_french(textToTranslate)
+    text_to_translate = request.args.get('textToTranslate')
+    return translator.english_to_french(text_to_translate)
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
+def french_to_english():
     """
     This represents the French to English Route
     """
 
-    textToTranslate = request.args.get('textToTranslate')
-    return translator.french_to_english(textToTranslate)
+    text_to_translate = request.args.get('textToTranslate')
+    return translator.french_to_english(text_to_translate)
 
 @app.route("/")
-def renderIndexPage():
+def render_index_page():
     """
     This renders the file index.html
     """
